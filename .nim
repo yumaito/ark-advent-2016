ecd9501e81ce5aae4d76aa1ae5315b7283205500
Masayuki Matsuki <y.songmu@gmail.com>
2011-10-11 20:21:00 +0900

    syntacはJSでやる方針に

--- .nim
+++ .nim
@@ -7,17 +7,6 @@ plugins:
   - module: Meta
   - module: Markdown
 
-  - module: EmacsColor
-    config:
-      filter: '(<(?:h|div|p|html|span|table|tr|td))'
-      mode: html
-
-  - module: EmacsColor
-    config:
-      filter: '(?:my \(?[\$\@\%]|use Ark|use [0-9a-zA-Z_:]+;|__PACKAGE__|\$[\w_]+->)'
-      mode: cperl
-
-  - module: AutoIndex
   - module: AutoIndex
     config:
       filename: index.xml
